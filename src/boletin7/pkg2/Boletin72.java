e7709624016f00c060f73283e405d6a908c34f4b
Toshiba <Toshiba@192.168.0.18>
2015-11-03 01:01:02 +0100

    Aplicacion terminada

---
+++ src/boletin7/pkg2/Boletin72.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin7.pkg2;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class Boletin72 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Condicion par1=new Condicion();
+        short num1=par1.getNumero();
+        short num2=par1.getNumero();
+        par1.comparar(num1,num2);
+    
+    }
+    
+}
